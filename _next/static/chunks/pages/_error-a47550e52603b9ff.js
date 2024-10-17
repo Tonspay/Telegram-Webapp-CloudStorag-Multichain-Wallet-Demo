59863c9d7d27ce7b16cc9915ebd8bfff1a611ee3
Your Name <you@example.com>
2024-10-17 08:35:39 +0000

    build

---
+++ _next/static/chunks/pages/_error-a47550e52603b9ff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{36556:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(67634)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=36556)}),_N_E=n.O()}]);
